737905d4e1d406d89ee9ff42ff6471bdbc37e10e
Alexander Moiseyev <moiseyev@dhcp175-221-red.yandex.net>
2010-03-26 20:45:40 +0300

    Add typograf function

--- Typography.tmbundle/russian_typography.rb
+++ Typography.tmbundle/russian_typography.rb
@@ -1,30 +1,29 @@
-#!/usr/bin/env ruby -wKU
-
 require 'net/http'
 require 'cgi'
 
-STDOUT.sync = true
-page_text = STDIN.read
+def typograf(params)
+  STDOUT.sync = true
+  input_text = STDIN.read
 
-post_args = <<SOAPBODY
+  post_args = <<SOAPBODY
 <?xml version="1.0" encoding="UTF-8"?>
 <soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
     <soap:Body>
-      <ProcessText xmlns="http://typograf.artlebedev.ru/webservices/">
-          <text>#{CGI.escapeHTML(page_text)}</text>
-          <entityType>4</entityType>
-          <useBr>0</useBr>
-          <useP>1</useP>
-          <maxNobr>3</maxNobr>
-      </ProcessText>
+        <ProcessText xmlns="http://typograf.artlebedev.ru/webservices/">
+            <text>#{CGI.escapeHTML(input_text)}</text>
+            <entityType>#{params[:entity_type] || 0}</entityType>
+            <useBr>#{params[:use_br] || 0}</useBr>
+            <useP>#{params[:use_p] || 0}</useP>
+            <maxNobr>3</maxNobr>
+        </ProcessText>
     </soap:Body>
 </soap:Envelope>
 SOAPBODY
 
-Net::HTTP.start('typograf.artlebedev.ru') do |query|
-  response = query.post('/webservices/typograf.asmx', post_args, {'Content-Type' => 'application/x-www-form-urlencoded'})
-  response.body.scan(%r{<ProcessTextResult>(.*?)</ProcessTextResult>}m) do |result|
-    puts result
-    #puts CGI.unescapeHTML(result.to_s)
+  Net::HTTP.start('typograf.artlebedev.ru') do |query|
+    response = query.post('/webservices/typograf.asmx', post_args, {'Content-Type' => 'application/x-www-form-urlencoded'})
+    response.body.scan(%r{<ProcessTextResult>(.*?)</ProcessTextResult>}m) do |result|
+      return result.to_s
+    end
   end
 end
